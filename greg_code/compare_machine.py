f81c8df33ea6dfbecb289a102bfa51f9a6bab773
Greg <57800288+MachineLearningUCG2019@users.noreply.github.com>
2019-12-11 17:25:16 +0100

    folder for my code

---
+++ greg_code/compare_machine.py
@@ -0,0 +1,32 @@
+#import numpy as np, matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.svm import SVC #support vector machine
+from sklearn.naive_bayes import GaussianNB  #Gauss
+from sklearn.linear_model import LogisticRegression #Logistic Regression
+from sklearn.datasets import load_breast_cancer
+
+machines = {
+  "SVC":SVC(kernel="linear"),
+  "Gauss":GaussianNB(),
+  "LR":LogisticRegression(solver="liblinear")
+}
+
+def main():
+  dataset = load_breast_cancer()
+  X = dataset.data
+  Y = dataset.target
+  x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+  scores = {}
+  for m in machines:
+    print("running machine:",m)
+    machines[m].fit(x_train,y_train)
+    y_pred = machines[m].predict(x_test)
+    score = metrics.accuracy_score(y_test,y_pred)
+    scores[m] = score
+    print("score:",score)
+    print("confusion matrix:\n",metrics.confusion_matrix(y_test,y_pred))
+    print("\n")
+  return scores
+
+main()
